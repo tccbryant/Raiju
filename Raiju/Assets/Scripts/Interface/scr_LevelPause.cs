a3b52a7d430c667c17f5979a30e978160fea0e95
Thomas Bryant <tccbryant@gmail.com>
2017-02-24 17:44:16 -0800

    Main Menu

---
+++ Raiju/Assets/Scripts/Interface/scr_LevelPause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scr_LevelPause : MonoBehaviour {
+
+	public Canvas cvs_Paused;
+
+	private bool paused = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Pause")) // pause game
+			paused = !paused;
+
+		if (paused) {
+			cvs_Paused.enabled = true;
+		} else {
+			cvs_Paused.enabled = false;
+		}
+			
+	}
+
+	public void btn_Resume_OnClick(){
+		paused = !paused;
+	}
+
+	public void btn_Options_OnClick(){
+
+	}
+
+	public void btn_QuitToMenu_OnClick(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
---
+++ Raiju/Assets/Scripts/Interface/scr_MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_MainMenu : MonoBehaviour
+{
+    // Use this for initialization
+    void Start() {
+		
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void btn_NewGame_OnClick() {
+        SceneManager.LoadScene("tile_test");
+    }
+
+	public void btn_Options_OnClick() {
+
+	}
+
+	public void btn_Exit_OnClick() {
+		Application.Quit ();
+	}
+
+}
